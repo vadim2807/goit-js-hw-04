318d56d0673949e97fac3131de2923db06a64760
LanaStudio <196567654+vadim2807@users.noreply.github.com>
2025-05-30 05:53:57 +0300

    первая сдача

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,29 @@
-function slugify(title) {
-    title = title.toLowerCase();
-    const arr = title.split(" ");
-    return arr.join("-");
+function isEnoughCapacity(products, containerSize) {
+    let summ = 0;
+    for (const key in products) {
+        summ += products[key];
+    }
+    if (summ > containerSize) {
+        return false;
+    } else {
+        return true;
+    }
 }
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
-console.log("----------------------");
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
+
+ console.log("----------------------");
--- js/task-2.js
+++ js/task-2.js
@@ -1,24 +1,38 @@
-function makeArray(firstArray, secondArray, maxLength) {
-    let i = 0;
-    let thirdArray = [];
-    while(i < maxLength && i < firstArray.length) {
-      thirdArray.push(firstArray[i]);
-      i++;
-    }
-  let j = 0;
-  while (thirdArray.length < maxLength && j < secondArray.length) {
-    thirdArray.push(secondArray[j]);
-    j++;
+function calcAverageCalories(days) {
+  let numDays = days.length;
+  let numCals = days.map(day => day.calories).reduce((prevCal, cal) => prevCal + cal, 0);
+  if (numDays === 0) {
+    numDays = 1;
   }
-
-  return thirdArray;
+  return numCals / numDays;
 }
 
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
 
 console.log("----------------------");
